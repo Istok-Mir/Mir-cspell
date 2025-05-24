5412cceee629bd8b20d655df19cf5a91ab8de783
Предраг Николић <idmpepe@gmail.com>
2025-05-24 21:10:00 +0200

    First commit

---
+++ main.py
@@ -0,0 +1,72 @@
+from __future__ import annotations
+import sublime_plugin
+from Mir.types import URI, DocumentUri, TextEdit
+from Mir import LanguageServer, mir
+from typing import Dict, List, Optional, Tuple, TypedDict
+from Mir.runtime import deno
+from Mir.api import ActivityIndicator
+from Mir.package_storage import PackageStorage, run_command
+import sublime
+
+
+server_storage = PackageStorage(__package__, tag='0.0.1', sync_folder="./language-server")
+
+
+class CspellLanguageServer(LanguageServer):
+    name='cSpell'
+    activation_events={
+        'selector': 'source.dosbatch | source.c | source.c++ | source.objc | source.objc++ | source.clojure | source.cs | source.cake | source.css | source.dart | source.diff | source.dockerfile | source.elixir | source.erlang | source.fsharp | text.git-commit | source.go | source.gomod | source.graphql | source.haskell | text.html.basic | source.ini | source.java | source.jsx | source.js.react | source.js | source.tsx | source.ts | source.json | source.julia | text.tex.latex | source.less | source.lua | source.makefile | text.html.markdown | source.perl | text.html.twig | text.blade | embedding.php | text.plain | source.powershell | source.python | source.r | source.ruby | source.rust | source.scala | source.scss | source.sql | source.swift | text.html.vue | text.xml | text.html.svelte | source.yml | source.yaml',
+    }
+    settings_file="Mir-cspell.sublime-settings"
+
+    async def activate(self):
+        # setup runtime and install dependencies
+        await deno.setup()
+        server_path = server_storage / "language-server" / "_server" / 'main.cjs'
+        server_node_modules = server_storage / "language-server" / "node_modules"
+        if not server_node_modules.exists():
+            with ActivityIndicator(sublime.active_window(), f'installing {self.name}'):
+                await run_command([deno.path, "install"], cwd=str(server_storage / "language-server"))
+
+        async def on_workspace_config_for_document(params: WorkspaceConfigForDocumentRequest) -> WorkspaceConfigForDocumentResponse:
+            # It looks like this method is necessary to enable code actions...
+            return {
+                'uri': None,
+                'workspaceFile': None,
+                'workspaceFolder': None,
+                'words': {},
+                'ignoreWords': {}
+            }
+        self.on_request('onWorkspaceConfigForDocumentRequest', on_workspace_config_for_document)
+
+        await self.connect('stdio', {
+            'cmd': [deno.path, 'run', '-A', server_path, '--stdio'],
+        })
+
+
+WorkspaceConfigForDocumentRequest = TypedDict('WorkspaceConfigForDocumentRequest', {
+    'uri': DocumentUri
+})
+
+FieldExistsInTarget = Dict[str, bool]
+
+WorkspaceConfigForDocumentResponse = TypedDict('WorkspaceConfigForDocumentResponse', {
+    'uri': Optional[DocumentUri],
+    'workspaceFile': Optional[URI],
+    'workspaceFolder': Optional[URI],
+    'words': FieldExistsInTarget,
+    'ignoreWords': FieldExistsInTarget
+})
+
+DocumentVersion = int
+EditTextArguments = Tuple[URI, DocumentVersion, List[TextEdit]]
+
+
+mir.commands.register_command('cSpell.editText', 'cspell_edit_text')
+
+class CspellEditTextCommand(sublime_plugin.TextCommand):
+    def run(self, edit, arguments: EditTextArguments):
+        _uri, document_version, text_edits = arguments
+        self.view.run_command('mir_apply_text_edits', {
+            'text_edits': text_edits
+        })
